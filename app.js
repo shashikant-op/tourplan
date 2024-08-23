04eb86bec1d27612acdff3132a0692ca0dc5e313
shashikant-op <sharmashashikantqwe@gmail.com>
2024-08-23 20:41:33 +0530

    authenticating adding

--- app.js
+++ app.js
@@ -23,8 +23,136 @@ main().then((res)=>{
 async function main(){
     mongoose.connect('mongodb://127.0.0.1:27017/hotel-list');
 }
+
+//authentication
+
+const mysql=require("mysql2");
+
+
+
+const connection=mysql.createConnection(
+    {
+        host:"localhost",
+        user:"root",
+        database:"app",
+        password:"Mysql12"
+
+    }
+);
+//fake data generator
+
+//  generator=()=>{
+//   return {
+// id: faker.string.uuid(),
+   
+//   };
+// }
+// let id =generator().id;
+// console.log(id);
+
+
+let q=`select*from users`;
+connection.query(q,(err,result)=>{
+    if (err) throw err;
+    console.log("sucess");
+    
+});
+
+
+//making route
 app.get("/",(req,res)=>{
-    res.render("listings/home.ejs");
+    res.render("signup.ejs");
+})
+app.get("/login",(req,res)=>{
+    isaunthenticated=false;
+    res.render("signin.ejs");
+   
+
+})
+
+app.patch("/",(req,res)=>{
+   let{mail:email,password:password,username:name,id:id}=req.body;
+    console.log(id);
+    let q=`insert into users values( "${id}","${name}","${email}","${password}")`;
+    connection.query(q,(err,result)=>{
+        if (err) {
+            res.send("something wrong in database: your id is to long");
+        }
+        res.render("signin.ejs");  
+        
+    })
+})
+let username;
+let userid;
+
+let isaunthenticated=false;
+    app.patch("/login/profile",(req,res)=>{
+        let{gmail:usermail,userpassword:loginpassword}=req.body;
+        let q2=`select*from users where email="${usermail}" `;
+       
+        connection.query(q2,(err,result)=>{
+          
+        let user=result[0];
+           // cheking password 
+           
+           let rpassword=result[0].password;
+           console.log(rpassword);
+           console.log(loginpassword);
+                        if(rpassword===loginpassword){
+                            isaunthenticated = true;
+                            username=user.name;
+                            userid=user.id;
+                          res.redirect("/home");
+                        }else{
+                            res.render("wrongpass.ejs");
+                        }
+    
+           
+           
+      })
+   
+     })
+   
+    
+     console.log(username);
+     
+     app.use((req,res,next)=>{
+        if(isaunthenticated){
+            console.log("authenticated");
+            next();
+        }
+        else{
+            console.log("your pass may be wrong");
+           res.redirect("/login");
+        }
+     })
+
+
+
+
+
+ 
+
+app.get("/logout",(req,res)=>{
+    isaunthenticated=false;
+    res.redirect("/login");
+   
+})
+app.get("/profile",(req,res)=>{
+     let q2=`select*from users where id="${userid}" `;
+     connection.query(q2,(err,result)=>{
+       let user=result[0];
+        res.render("welcome.ejs",{user});
+     })
+
+   
+})
+
+
+//main page
+app.get("/home",(req,res)=>{
+    res.render("listings/home.ejs",{username});
+   
 })
 //test route
 // app.get("/listingstest",async(req,res)=>{
@@ -42,17 +170,18 @@ app.get("/",(req,res)=>{
 //index route
 app.get("/listings",async(req,res)=>{
   const listings= await hotellisting.find({});
-res.render("listings/index.ejs",{listings});
+res.render("listings/index.ejs",{listings,username});
     
 })
+//show route
 app.get("/listings/show/:id",async(req,res)=>{
    let {id}=req.params;
     const details =await hotellisting.findById(id);
-    res.render("listings/show.ejs",{details});
+    res.render("listings/show.ejs",{details,username});
 })
 //new route
 app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs");
+    res.render("listings/new.ejs",{username});
 })
 app.post("/listings",async(req,res)=>{
     const newlisting=new hotellisting(req.body.listing);
@@ -63,17 +192,18 @@ app.post("/listings",async(req,res)=>{
 
 })
 //edit route
+
 app.get("/listings/edit/:id",async(req,res)=>{
     let {id}=req.params;
     let details=await hotellisting.findById(id);
-    res.render("listings/update.ejs",{details});
+    res.render("listings/update.ejs",{details,username});
 })
 //update route
 
 app.put("/listings/:id",async(req,res)=>{
     let {id}=req.params;
     await hotellisting.findByIdAndUpdate(id,{...req.body});
-    res.redirect("/listings",{id});
+    res.redirect("/listings",{id,username});
 })
 //delete route
 app.delete("/listings/:id",async(req,res)=>{
